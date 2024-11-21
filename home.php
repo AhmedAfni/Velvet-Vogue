daabbb71de26e525714ff767825b68fd1ffd5fd0
AhmedAfni <ahmedafni86@gmail.com>
2024-11-21 20:17:20 +0530

    Implement search functionality

--- home.php
+++ home.php
@@ -25,10 +25,6 @@
                 </li>
             </ul>
 
-            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-            </form>
-
             <div class="text-end">
                 <button type="button" class="btn btn-warning me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
                     REGISTER
@@ -460,6 +456,7 @@ var carousel = new bootstrap.Carousel(myCarousel, {
         interval: 0, 
         ride: 'carousel' 
     });
+
 
 </script>
 </html>
